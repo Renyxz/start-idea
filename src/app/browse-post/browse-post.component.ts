9d5b2d533d444fb02cd9a5cc01953c049d02c2ab
Ren <ren.yxz.dev@gmail.com>
2017-11-29 18:06:30 -0500

    Added new component for browsing post

---
+++ src/app/browse-post/browse-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-post',
+  templateUrl: './browse-post.component.html',
+  styleUrls: ['./browse-post.component.scss']
+})
+export class BrowsePostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
