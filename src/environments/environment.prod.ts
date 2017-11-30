2eec3b8caf0fe45dcbd4b607f87278c20be0a967
Ren <ren.yxz.dev@gmail.com>
2017-11-30 16:59:34 -0500

    Added missing firebaseConfig property which solved prod build error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCYj3cu66XiuOIXw_u6BTloyn1msHibSOM',
+    authDomain: 'start-idea-app.firebaseapp.com',
+    databaseURL: 'https://start-idea-app.firebaseio.com',
+    projectId: 'start-idea-app',
+    storageBucket: '',
+    messagingSenderId: '714600155333'
+  }
 };
